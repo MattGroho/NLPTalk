29e1858fa5d26e9649ef7cdb4f5f4122b32c7244
MattBcool <mattgroho@gmail.com>
2022-03-21 22:10:07 -0400

    Added Vocab function and a way to get list shape dynamically

--- models/MLNLP.py
+++ models/MLNLP.py
@@ -1,3 +1,7 @@
+import numpy as np
+import tensorflow as tf
+
+
 class MLNLP:
     model = None
 
@@ -6,3 +10,9 @@ class MLNLP:
 
     def evaluate(self, text):
         pass
+
+    # Gets shape dynamically to pass to model
+    def shape_list(self, x):
+        static_shape = x.shape.as_list()
+        dynamic_shape = tf.shape(x)
+        return [dynamic_shape[i] if s is None else s for i, s in enumerate(static_shape)]
--- spark/Tokenizer.py
+++ spark/Tokenizer.py
@@ -4,6 +4,7 @@ import spacy
 class Tokenizer:
 
     nlp = None
+    vocab = {}
 
     def __init__(self):
         pass
@@ -20,3 +21,19 @@ class Tokenizer:
             print(token.morph)
 
         return doc_list
+
+    # Vocabularize a list of words by adding them to the vocabulary if they do not already exist
+    def vocabularize(self, text):
+        for i, word in enumerate(text):
+            word = word.lower()
+
+            # Adds a new word to the vocab list if it does not already exist
+            if not self.vocab.has_key(word):
+                self.vocab[word] = len(self.vocab)
+
+            # Replace text with vocab index
+            text[i] = self.vocab[word]
+
+    # Saves the current vocabulary list
+    def save_vocab(self):
+        pass
