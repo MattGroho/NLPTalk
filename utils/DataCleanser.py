bf9088ff9b695046bacc9a80ea9f3f206c0b1a19
MattBcool <mattgroho@gmail.com>
2022-04-07 16:39:11 -0400

    Added DataCleanser and imported EtownData file provided by Luke M.

---
+++ utils/DataCleanser.py
@@ -0,0 +1,15 @@
+import docx2txt
+
+
+def save_doc_as_txt(doc_loc, new_doc_loc):
+    text = docx2txt.process(doc_loc)
+
+    with open(new_doc_loc, 'w') as text_file:
+        print(text, file=text_file)
+
+
+def read_doc(doc_loc):
+    with open(doc_loc, 'r') as text_file:
+        return text_file.read()
+
+    return None
